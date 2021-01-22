e5fe07f3baa9cee7fcba665141c3a41b0f6f4df2
axw <1425971253@qq.com>
2021-01-22 13:06:03 +0800

    create interface

---
+++ 03-spring-initializr/src/main/java/com/cy/InitializrApplication.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*springboot 工程默认启动类*/
+@SpringBootApplication
+public class InitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrApplication.class, args);
+    }
+
+}
